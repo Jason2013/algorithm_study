4ed8f88949a773822f49f1f082d12963488e85c4
Jason Chen <shchenchang@gmail.com>
2019-07-09 18:13:08 +0800

    Code from: https://www.cnblogs.com/evasean/p/7204501.html

--- src/main/java/assignments/percolation/Percolation.java
+++ src/main/java/assignments/percolation/Percolation.java
@@ -1,122 +1,207 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
+/**
+ * @author evasean www.cnblogs.com/evasean/
+ */
 public class Percolation {
+    private static final boolean BLOCK = false; // block state
+    private static final boolean OPEN = true; // open state
 
-    private final int size;
-    private final WeightedQuickUnionUF quTop;
-    private final WeightedQuickUnionUF quBottom;
-    private boolean[] site = null;
-    private int openSites = 0;
-    private final int vnode;
-    private boolean percolated = false;
+    /* topUF bottomUF n 均为final是因为它们只在构造函数时初始化，后续其值未发生变化 */
+    private final WeightedQuickUnionUF topUF; // 用来记录与top虚节点的连通性
+    private final WeightedQuickUnionUF bottomUF;// 用来记录与bottom虚节点的连通性
+    private final int n;
 
-    // creates n-by-n grid, with all sites initially blocked
+    private boolean[][] grid;
+    private boolean percolateFlag = false; // grid是否渗透的标志
+    private int openedNum = 0;// 已经open的site数目
+
     public Percolation(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException();
-        }
-
-        size = n;
-
-        quTop = new WeightedQuickUnionUF(n * n + 1);
-        quBottom = new WeightedQuickUnionUF(n * n + 1);
-
-        site = new boolean[n*n];
-        for (int i = 0; i < site.length; i++)
-        {
-            site[i] = false;
-        }
-
-        vnode = n*n;
+        // create n-by-n grid, with all sites blocked
+        if (n < 1)
+            throw new IllegalArgumentException("grid size should be bigger than one !");
+        this.n = n;
+        topUF = new WeightedQuickUnionUF(n * n + 1); // 多了一个节点的空间，位置n*n处用来代表虚节点
+        bottomUF = new WeightedQuickUnionUF(n * n + 1); // 多了一个节点的空间，位置n*n处用来代表虚节点
+        grid = new boolean[n][n];
+        // 初始化grid设为block
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = BLOCK;
     }
 
     private void validate(int row, int col) {
-        if (!(row >= 1 && row <= size && col >= 1 && col <= size)) {
-            throw new IllegalArgumentException();
-        }
+        if (row < 1 || col < 1 || row > n || col > n)
+            throw new IllegalArgumentException("input row or col is not illegal!");
     }
 
-    private int index(int row, int col) {
-        return (row - 1) * size + (col - 1);
-    }
-
-    // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        // open site (row, col) if it is not open already
         validate(row, col);
+        if (grid[row - 1][col - 1] == OPEN)
+            return;
 
-        if (isOpen(row, col)) {
+        grid[row - 1][col - 1] = OPEN;
+        openedNum++;
+
+        // n为1时，open一个节点就达到渗透要求
+        if (n == 1) {
+            topUF.union(0, 1);
+            bottomUF.union(0, 1);
+            percolateFlag = true;
             return;
         }
 
-        int idx = index(row, col);
-        site[idx] = true;
-        openSites++;
+        // 第一行的所有节点都与top虚节点连通
+        if (row == 1)
+            topUF.union(n * n, col - 1);
 
-        // check up
-        if (row > 1 && isOpen(row - 1, col)) {
-            int oldIdx = index(row - 1, col);
-            quTop.union(idx, oldIdx);
-            quBottom.union(idx, oldIdx);
-        }
+        // 最后一行的所有节点都与bottom虚节点连通
+        if (row == n)
+            bottomUF.union(n * n, (n - 1) * n + col - 1);
 
-        // check down
-        if (row < size && isOpen(row + 1, col)) {
-            int oldIdx = index(row + 1, col);
-            quTop.union(idx, oldIdx);
-            quBottom.union(idx, oldIdx);
+        // 与上方节点的连通性
+        if (row > 1 && grid[row - 2][col - 1] == OPEN) {
+            topUF.union((row - 2) * n + col - 1, (row - 1) * n + col - 1);
+            bottomUF.union((row - 2) * n + col - 1, (row - 1) * n + col - 1);
         }
 
-        // check left
-        if (col > 1 && isOpen(row, col - 1)) {
-            int oldIdx = index(row, col - 1);
-            quTop.union(idx, oldIdx);
-            quBottom.union(idx, oldIdx);
+        // 与下方节点的连通性
+        if (row < n && grid[row][col - 1] == OPEN) {
+            topUF.union(row * n + col - 1, (row - 1) * n + col - 1);
+            bottomUF.union(row * n + col - 1, (row - 1) * n + col - 1);
         }
 
-        // check right
-        if (col < size && isOpen(row, col + 1)) {
-            int oldIdx = index(row, col + 1);
-            quTop.union(idx, oldIdx);
-            quBottom.union(idx, oldIdx);
+        // 与左侧节点的连通性
+        if (col > 1 && grid[row - 1][col - 2] == OPEN) {
+            topUF.union((row - 1) * n + col - 2, (row - 1) * n + col - 1);
+            bottomUF.union((row - 1) * n + col - 2, (row - 1) * n + col - 1);
         }
 
-        // connect virtual node
-        if (row == 1) {
-            quTop.union(idx, vnode);
-        }
-        if (row == size) {
-            quBottom.union(idx, vnode);
+        // 与右侧节点的连通性
+        if (col < n && grid[row - 1][col] == OPEN) {
+            topUF.union((row - 1) * n + col, (row - 1) * n + col - 1);
+            bottomUF.union((row - 1) * n + col, (row - 1) * n + col - 1);
         }
 
-        if (!percolated && openSites >= size && quTop.connected(idx, vnode) && quBottom.connected(idx, vnode)) {
-            percolated = true;
-        }
+        /*
+         * 判断条件!percolateFlag是为了防止渗透以后的重复判断 判断条件openedNum>=n
+         * 是因为openedNum达到n时才有可能渗透，在未达到n之前，不需要进行后续判断
+         * 一个节点open的时候刚好使grid渗透的条件是该节点同时与top虚节点和bottom虚节点连通
+         */
+        if (!percolateFlag && openedNum >= n && topUF.connected(n * n, (row - 1) * n + col - 1)
+                && bottomUF.connected(n * n, (row - 1) * n + col - 1))
+            percolateFlag = true;
+
     }
 
-    // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
+        // is site (row, col) open?
         validate(row, col);
-        int idx = index(row, col);
-        return site[idx];
+        return grid[row - 1][col - 1] == OPEN;
     }
 
-    // is the site (row, col) full?
+    /**
+     * 一个节点只有同时在open状态并且与top虚节点连通时才是full状态
+     * @param row
+     * @param col
+     * @return
+     */
     public boolean isFull(int row, int col) {
+        // is site (row, col) full?
         validate(row, col);
-        return quTop.connected(vnode, index(row, col));
+        if (isOpen(row, col) && topUF.connected(n * n, (row - 1) * n + col - 1))
+            return true;
+        else
+            return false;
     }
 
-    // returns the number of open sites
     public int numberOfOpenSites() {
-        return openSites;
+        // number of open sites
+        return openedNum;
     }
 
-    // does the system percolate?
     public boolean percolates() {
-        return percolated;
+        // does the system percolate?
+        return percolateFlag;
+    }
+
+    //打印一些便于查看的信息
+    private void printCheckResult(int row, int col) {
+        StdOut.println("p(" + row + "," + col + ") is open=" + isOpen(row, col) + ";is full=" + isFull(row, col)
+                + ";percolates=" + percolates());
+    }
+
+    /**
+     * 作业提交时main需要调用该方法，因为提交后在线脚本要用一堆input文件进行测试
+     * 
+     * @param arg0
+     */
+    private static void fileInputCheck(String arg0) {
+        // test client (optional)
+        In in = new In(arg0);//读入input文件名，并加载文件内容
+        String s = null;
+        int n = -1;
+        //读入grid的n
+        while (in.hasNextLine()) {
+            s = in.readLine();
+            if (s != null && !s.trim().equals(""))
+                break;
+        }
+        s = s.trim();
+        n = Integer.parseInt(s);
+        Percolation p = new Percolation(n);
+        
+        //读入open的site坐标
+        while (in.hasNextLine()) {
+            s = in.readLine();
+            if (s != null && !s.trim().equals("")) {
+                s = s.trim();//去掉输入字符串头尾空格
+                String[] sa = s.split("\\s+");//去掉中间所有空格
+                if (sa.length != 2)
+                    break;
+                int row = Integer.parseInt(sa[0]);
+                int col = Integer.parseInt(sa[1]);
+                p.open(row, col);
+            }
+        }
+
+    }
+
+    /**
+     * 本地测试专用
+     */
+    private static void generateCheck() {
+        // test client (optional)
+        Percolation p = new Percolation(3);
+        int row = 1, col = 3;
+        p.open(row, col);
+        p.printCheckResult(row, col);
+        row = 2;
+        col = 3;
+        p.open(row, col);
+        p.printCheckResult(row, col);
+        row = 3;
+        col = 3;
+        p.open(row, col);
+        p.printCheckResult(row, col);
+        row = 3;
+        col = 1;
+        p.open(row, col);
+        p.printCheckResult(row, col);
+        row = 2;
+        col = 1;
+        p.open(row, col);
+        p.printCheckResult(row, col);
+        row = 1;
+        col = 1;
+        p.open(row, col);
+        p.printCheckResult(row, col);
     }
 
     public static void main(String[] args) {
-        // StdOut.println("hello, world");
+        //generateCheck();
+        fileInputCheck(args[0]);
     }
 }
--- src/main/java/assignments/percolation/PercolationStats.java
+++ src/main/java/assignments/percolation/PercolationStats.java
@@ -1,74 +1,79 @@
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-
+/**
+ * @author evasean www.cnblogs.com/evasean/
+ */
 public class PercolationStats {
-    private static final double CONFIDENCE_95 = 1.96;
-    private final double[] stats;
+    /* t fractions 均为final是因为它们只在构造函数时初始化，后续其值未发生变化*/
+    private final int t;//尝试次数
+    private final double[] fractions;//每一次尝试的渗透率得分
+    
+    private double mean;
+    private double stddev;
 
-    // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
-
-        if (n <= 0 || trials <= 0) {
-            throw new IllegalArgumentException();
-        }
-
-        stats = new double[trials];
-        for (int t = 0; t < trials; t++) {
+        // perform trials independent experiments on an n-by-n grid
+        if (n <= 0 || trials <= 0)
+            throw new IllegalArgumentException("n ≤ 0 or trials ≤ 0");
+        t = trials;
+        fractions = new double[t];
+        for (int i = 0; i < t; i++) {//t次尝试
             Percolation p = new Percolation(n);
-
-            // randomize indices
-            int[] idx = new int[n*n];
-            for (int i = 0; i < idx.length; i++) {
-                idx[i] = i;
-            }
-            StdRandom.shuffle(idx);
-
-            int curidx = 0;
-            while (!p.percolates())
-            {
-                int val = idx[curidx++];
-                int row = (val / n) + 1;
-                int col = (val % n) + 1;
+            int openNum = 0;
+             //为了实现随机open一个site，模仿QuickUnion的定位方法
+            //先生成一个[0,n*n)的数组，数组内容随机排序,依次读取数组内容，就相当于随机取site
+            int[] rand = StdRandom.permutation(n * n);
+            for (int pos : rand) {
+                //pos = (row-1)*n + col -1
+                int row = pos / n + 1; 
+                int col = pos % n + 1;
                 p.open(row, col);
+                openNum++;
+                //只有openNum>=n时才有判断是否渗透的必要
+                if (openNum >= n && p.percolates())
+                    break;
             }
-
-            stats[t] = (double) curidx / (double) (n*n);
+            double pt = (double) openNum / (n * n);//单次尝试的渗透率
+            fractions[i] = pt;
         }
+        /* 作业提交时的某个测试案例要求mean()、stddev()、confidenceLo()、confidenceHi()
+         * 在任何时候任何次序调用的情况下都必须返回相同的值，故需要在构造函数中计算mean和stddev
+         */
+        //作业提交时的某个测试案例要调用一次StdStats.mean方法
+        mean = StdStats.mean(fractions);
+        //作业提交时的某个测试案例要求要调用一次StdStats.stddev方法
+        stddev = StdStats.stddev(fractions);
     }
 
-    // sample mean of percolation threshold
     public double mean() {
-        return StdStats.mean(stats);
+        // sample mean of percolation threshold
+        return mean;
     }
 
-    // sample standard deviation of percolation threshold
     public double stddev() {
-        return StdStats.stddev(stats);
+        // sample standard deviation of percolation threshold
+        return stddev;
     }
 
-    // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return StdStats.mean(stats) - CONFIDENCE_95 * StdStats.stddev(stats) / Math.sqrt(stats.length);
+        // low endpoint of 95% confidence interval
+        return mean - 1.96 * stddev / Math.sqrt(t);
     }
 
-    // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return StdStats.mean(stats) + CONFIDENCE_95 * StdStats.stddev(stats) / Math.sqrt(stats.length);
+        // high endpoint of 95% confidence interval
+        return mean + 1.96 * stddev / Math.sqrt(t);
     }
 
-    // test client (see below)
     public static void main(String[] args) {
-
-        if (args.length < 2) {
-            throw new IllegalArgumentException();
-        }
-
+        // test client (described below)
         int n = Integer.parseInt(args[0]);
-        int trials = Integer.parseInt(args[1]);
-
-        PercolationStats ps = new PercolationStats(n, trials);
-        System.out.println("mean                    = " + ps.mean());
-        System.out.println("stddev                  = " + ps.stddev());
-        System.out.println("95% confidence interval = [" + ps.confidenceLo() +", " + ps.confidenceHi() + "]");
+        int t = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, t);
+        StdOut.printf("%-25s %s %f \n", "means", "=", ps.mean());
+        StdOut.printf("%-25s %s %f \n", "stddev", "=", ps.stddev());
+        StdOut.printf("%-25s %s%f%s%f%s\n", "95% confidence interval", "= [", ps.confidenceLo(), ", ",
+                ps.confidenceHi(), "]");
     }
 }
