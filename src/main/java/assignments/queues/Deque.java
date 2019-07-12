ffec56da4c27ee4787e73aab515a837a4532b418
Jason Chen <shchenchang@gmail.com>
2019-07-12 17:27:39 +0800

    Add `Deque.java` file.

---
+++ src/main/java/assignments/queues/Deque.java
@@ -0,0 +1,50 @@
+
+import java.util.Iterator;
+//import edu.princeton.cs.algs4.StdRandom;
+
+public class Deque<T> implements Iterable<T>
+
+    // construct an empty deque
+  public Deque() {}
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+      return 0;
+    }
+
+    // add the item to the front
+    public void addFirst(T item) {
+
+    }
+
+    // add the item to the back
+    public void addLast(T item) {
+
+    }
+
+    // remove and return the item from the front
+    public T removeFirst() {
+        return null;
+    }
+
+    // remove and return the item from the back
+    public T removeLast() {
+        return null;
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+    	System.out.println("Hello, world!");
+    }
+
+}
