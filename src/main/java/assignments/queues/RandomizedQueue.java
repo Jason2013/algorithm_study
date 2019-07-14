15976c15df57fd007390a2b764a9a4bc576f2ce3
Jason Chen <shchenchang@gmail.com>
2019-07-14 14:42:03 +0800

    Implement `RandomizedQueue` class.

--- src/main/java/assignments/queues/RandomizedQueue.java
+++ src/main/java/assignments/queues/RandomizedQueue.java
@@ -1,30 +1,60 @@
 import java.util.Iterator;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
+	private final int initCapacity = 8;
+	private int _size;
+	private int _capacity;
+	private Item[] items;
     // construct an empty randomized queue
     public RandomizedQueue() {
-    	
+    	_size = 0;
+    	_capacity = initCapacity;
+    	items = (Item[]) new Object[_capacity];
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-    	return false;
+    	return _size == 0;
     }
 
     // return the number of items on the randomized queue
     public int size() {
-    	return 0;
+    	return _size;
+    }
+    
+    private void resize(int n) {
+    	assert n >= _size;
+    	Item[] newItems = (Item[]) new Object[n];
+    	for (int i = 0; i < _size; i++) {
+    		newItems[i] = items[i];
+    	}
+    	_capacity = n;
+    	items = newItems;
     }
 
     // add the item
     public void enqueue(Item item) {
-    	
+    	if (_size == _capacity) {
+    		resize(_capacity * 2);
+    	}
+    	items[_size] = item;
+    	_size++;
     }
 
     // remove and return a random item
     public Item dequeue() {
-    	return null;
+    	int idx = StdRandom.uniform(_size);
+    	Item ret = items[idx];
+    	items[idx] = items[--_size];
+    	
+    	if (_size < _capacity / 4) {
+    		resize(_capacity /2);
+    	}
+    	return ret;
+//    	return null;
     }
 
     // return a random item (but do not remove it)
