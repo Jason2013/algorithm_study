f1eebd1d5c4e699148bc47856b1033ff747e4bf5
Jason Chen <shchenchang@gmail.com>
2019-07-14 15:03:55 +0800

    Implement `RandomizedQueueIterator` class.

--- src/main/java/assignments/queues/RandomizedQueue.java
+++ src/main/java/assignments/queues/RandomizedQueue.java
@@ -24,7 +24,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public int size() {
     	return _size;
     }
-    
+
     private void resize(int n) {
     	assert n >= _size;
     	Item[] newItems = (Item[]) new Object[n];
@@ -49,22 +49,47 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     	int idx = StdRandom.uniform(_size);
     	Item ret = items[idx];
     	items[idx] = items[--_size];
-    	
+
     	if (_size < _capacity / 4) {
     		resize(_capacity /2);
     	}
     	return ret;
-//    	return null;
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
-    	return null;
+    	return items[StdRandom.uniform(_size)];
+    }
+
+    private class RandomizedQueueIterator<Item> implements Iterator<Item> {
+
+    	private int[] indices;
+    	private int cur;
+    	public RandomizedQueueIterator() {
+    		indices = new int[_size];
+    		for (int i = 0; i < indices.length; i++) {
+    			indices[i] = i;
+    		}
+    		StdRandom.shuffle(indices);
+    		cur = 0;
+    	}
+		@Override
+		public boolean hasNext() {
+			return cur < indices.length - 1;
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public Item next() {
+			Item ret = (Item) items[indices[cur++]];
+			return ret;
+			// TODO Auto-generated method stub
+		}
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-    	return null;
+    	return new RandomizedQueueIterator();
     }
 
     // unit testing (required)
