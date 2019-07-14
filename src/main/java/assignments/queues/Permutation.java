258b1adc59265830900499fc07bed2f9e82956a8
Jason Chen <shchenchang@gmail.com>
2019-07-14 22:49:46 +0800

    Add `Permutation.java`.

---
+++ src/main/java/assignments/queues/Permutation.java
@@ -0,0 +1,22 @@
+import edu.princeton.cs.algs4.StdIn;
+
+public class Permutation {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+
+        while (!StdIn.isEmpty()) {
+            rq.enqueue(StdIn.readString());
+            //
+        }
+        assert rq.size() >= k;
+
+        for (int i = 0; i < k; i++) {
+            System.out.println(rq.dequeue());
+        }
+
+    }
+
+}
