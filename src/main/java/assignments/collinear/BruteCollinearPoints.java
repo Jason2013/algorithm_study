ecbc3d645528f074308603904b61d63b0e4c2ceb
Jason Chen <shchenchang@gmail.com>
2019-07-17 07:29:44 +0800

    Add `BruteCollinearPoints` class.

---
+++ src/main/java/assignments/collinear/BruteCollinearPoints.java
@@ -0,0 +1,19 @@
+
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) { // finds all line segments containing 4 points
+    }
+
+    public int numberOfSegments() { // the number of line segments
+        return 0;
+    }
+
+    public LineSegment[] segments() { // the line segments
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
