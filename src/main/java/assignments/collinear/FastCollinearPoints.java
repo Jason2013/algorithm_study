36d104ac698812ffb35e5f2e93bec1a932c6fa0e
Jason Chen <shchenchang@gmail.com>
2019-07-30 18:18:37 +0800

    Add sample `main` code.

--- src/main/java/assignments/collinear/FastCollinearPoints.java
+++ src/main/java/assignments/collinear/FastCollinearPoints.java
@@ -1,5 +1,9 @@
 import java.util.Arrays;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 public class FastCollinearPoints {
 
     private int segmentCount = 0;
@@ -117,6 +121,33 @@ public class FastCollinearPoints {
 
         FastCollinearPoints bcp1 = new FastCollinearPoints(pts1);
         assert bcp1.numberOfSegments() == 3;
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 
 }
