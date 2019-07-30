860692c73a026107eb394168816ad76449a1e268
Jason Chen <shchenchang@gmail.com>
2019-07-30 15:24:23 +0800

    Add `FastCollinearPoints` skeleton code.

---
+++ src/main/java/assignments/collinear/FastCollinearPoints.java
@@ -0,0 +1,22 @@
+
+public class FastCollinearPoints {
+
+    public FastCollinearPoints(Point[] points)     // finds all line segments containing 4 or more points
+    {
+        //
+    }
+    public           int numberOfSegments()        // the number of line segments
+    {
+        return 0;
+    }
+    public LineSegment[] segments()                // the line segments
+    {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
