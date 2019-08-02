c800b424629cbc0a9602344eb528534ef48dcb26
Jason Chen <shchenchang@gmail.com>
2019-08-02 22:50:18 +0800

    Add sample data files.

---
+++ src/main/java/assignments/collinear/makezip.bat
@@ -0,0 +1,3 @@
+/* set ZIP_EXE=C:\chenchang\app\zip-3.0-bin\bin\zip.exe */
+set ZIP_EXE=D:\app\zip\3.0\bin\zip.exe
+%ZIP_EXE% collinear.zip Point.java BruteCollinearPoints.java FastCollinearPoints.java
